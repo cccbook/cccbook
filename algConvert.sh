c7b10378c1797db239e62cb79998db78afa7901e
ccckmit <ccckmit@gmail.com>
2025-01-17 15:51:13 +0800

    add alg physics book convert

---
+++ algConvert.sh
@@ -0,0 +1,8 @@
+./gpt2github.sh 演算法/不動點
+./gpt2github.sh 演算法/計算理論
+./gpt2github.sh 演算法/密碼學
+./gpt2github.sh 演算法/量子電腦
+./gpt2github.sh 演算法/資訊理論
+./gpt2github.sh 演算法/演算法
+./gpt2github.sh 演算法/演算法的數學
+./gpt2github.sh 演算法/蒙地卡羅
---
+++ physicsConvert.sh
@@ -0,0 +1,2 @@
+./gpt2github.sh 物理類/相對論
+./gpt2github.sh 物理類/量子力學
